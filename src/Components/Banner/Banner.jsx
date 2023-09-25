d71bca3d4bcd97509d4d5171d9916281ee8ededc
Foyez-Ahamed <foyez1435@gmail.com>
2023-09-25 19:54:07 +0600

    banner section added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,23 +1,24 @@
 
 const Banner = () => {
     return (
-        <div>
-{/* 
-              <div className="hero min-h-screen
-              " style={{backgroundImage: 'url(https://i.ibb.co/tPz3tK9/Rectangle-4281.png)', backgroundColor: 'white', opacity : 90}}>
-              <div>
-              <div className="text-center">
-                <h1 className="text-5xl">I Grow By Helping People In Need</h1>
-                
-                <div className="mt-8">
-                <input type="text" placeholder="Search here...." className="input input-bordered w-full max-w-xs" />
-                <button className="py-3 px-5 rounded-lg text-white bg-[#FF444A] ">Search</button>
-                </div>
-            </div>
-          </div>
-              </div> */}
-            
+       <div className="relative">
+       
+        <section style={{backgroundImage: 'url(https://i.ibb.co/x2fQdPv/Rectangle-4281.png)'}} className="bg-cover h-[70vh] bg-no-repeat object-cover bg-white opacity-10" >
+        </section>
+       
+        <div className=" flex justify-center items-center text-center">
+
+        <div className="absolute top-20">
+           <h1 className=" text-xl lg:text-4xl font-bold">I Grow By Helping People In Need</h1>
+
+           <input type="text" placeholder="Search here...." className="input input-bordered rounded-e-none lg:w-full max-w-xs mt-6" />
+           <button className="px-6 py-3 bg-[#FF444A] rounded-e-lg text-white">Search</button>
+         </div>
+
         </div>
+
+        </div>
+
     );
 };
 
