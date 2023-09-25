29bc7faa567e5f9edaf9e664380af992ae89584d
Foyez-Ahamed <foyez1435@gmail.com>
2023-09-25 12:08:02 +0600

    on clicking donate button show success message for donate added

--- src/Components/Donations/Donate/DonateCart/DonateCart.jsx
+++ src/Components/Donations/Donate/DonateCart/DonateCart.jsx
@@ -1,7 +1,33 @@
+import swal from "sweetalert";
 
 
 const DonateCart = ({donate}) => {
-    const {id, image, card_bg, category, category_bg, text_bg, title, price, description} = donate || {};
+    const {id, image, text_bg, title, price, description} = donate || {};
+
+    const handleDonate = () => {
+
+        const addToDonate = [];
+
+        const donations = JSON.parse(localStorage.getItem('Donate'));
+
+        if(!donations) {
+            addToDonate.push(donate);
+            localStorage.setItem('Donation', JSON.stringify(addToDonate));
+            swal("Good job!", "Donation added successfully!", "success");
+        }  
+
+        else{
+            const isDonateExists = donations.find(donation => donation.id === id);
+
+            if(!isDonateExists) {
+                addToDonate.push(...donations, donate);
+                localStorage.setItem('Donation', JSON.stringify(addToDonate));
+            }
+        }
+    }
+
+
+
     return (
         <div>
            <div className="mt-6">
@@ -9,7 +35,7 @@ const DonateCart = ({donate}) => {
             <div className="relative">
                 <img className="w-full h-[200px] lg:h-screen" src={image} alt="" />
                 <div>
-                <button style={{backgroundColor : text_bg}} className="absolute bottom-[20px] left-7 text-white px-6 py-2 rounded-md">Donate $ {price}</button>
+                <button onClick={handleDonate} style={{backgroundColor : text_bg}} className="absolute bottom-[20px] left-7 text-white px-6 py-2 rounded-md">Donate $ {price}</button>
                 </div>
             </div>
 
